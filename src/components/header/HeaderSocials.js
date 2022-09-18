1b6a6bcd9c24094cd1c8b3d3873de902e194ce56
AzizBoughattas <boughattas.medaziz@esprit.tn>
2022-09-18 13:32:50 +0100

    content changes

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -5,7 +5,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/med-aziz-boughattas-ab4807181/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
+        <a href='https://www.linkedin.com/in/aziz-boughattas-ab4807181/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
         <a href='https://github.com/AzizBoughattas' target="_blank" rel="noreferrer"><FaGithub /></a>
     </div>
   )
